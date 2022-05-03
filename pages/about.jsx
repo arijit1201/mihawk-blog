d116b713d523ebdfab14f4cfd50f5cabd3330e70
arijit1201 <arijit1201@gmail.com>
2022-05-03 09:34:20 +0530

    About page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import styles from '../styles/About.module.css'
 
 const About = () => {
   return (
-    <div>This is my About Section</div>
+    <div className={styles.container}>
+      <h1>About Us</h1>
+      <h2>What is CodeWalker Saga?</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque voluptatem quaerat illo, perferendis eius possimus nulla harum nam, culpa modi autem obcaecati distinctio ad earum. Nulla tempore, accusamus dolorem architecto, sunt est sapiente necessitatibus fugiat sed repellendus eligendi aspernatur omnis.</p>
+      <h2>Fields of proficiency</h2>
+      <ul>
+        <li>Service 1</li>
+        <li>Service 2</li>
+        <li>Service 3</li>
+        <li>Service 4</li>
+        <li>Service 5</li>
+      </ul>
+      <h2>Visions and Goals</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius voluptates incidunt quae. Vero est illo animi voluptate aspernatur ut modi neque, repellendus incidunt facere rerum ipsum assumenda saepe, possimus tenetur soluta illum? Earum numquam quia magni corrupti placeat consequatur hic doloremque. Blanditiis laudantium expedita, delectus minus eum velit quos veritatis saepe consequuntur doloribus eveniet non libero possimus id, ducimus harum?
+      </p>
+      <h2>Contact Us</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, excepturi, reprehenderit nemo ratione reiciendis labore unde enim perspiciatis placeat iste quisquam veniam similique suscipit necessitatibus, quidem ullam doloribus fuga porro.</p>
+    </div>
   )
 }
 
